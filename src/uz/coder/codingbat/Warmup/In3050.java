36b3c530a063f24dc7ea0059b2b8083a3c28c53a
mrazamat <azamat00189@mail.ru>
2019-01-19 10:23:39 +0500

    lastDigit

---
+++ src/uz/coder/codingbat/Warmup/In3050.java
@@ -0,0 +1,16 @@
+package uz.coder.codingbat.Warmup;
+
+// Given 2 int values, return true if they are both in the range 30..40 inclusive,
+// or they are both in the range 40..50 inclusive.
+public class In3050 {
+    public static void main(String[] args) {
+        System.out.println(in3050(30, 41    ));
+    }
+
+    private static boolean in3050(int a, int b) {
+
+        return true;
+    }
+
+}
+
