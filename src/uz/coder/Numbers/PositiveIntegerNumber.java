cc126c75859e4b0986c3e748ba0034b047b11067
mrazamat <azamat00189@mail.ru>
2019-01-23 13:18:32 +0500

    Java program to reverse bits of a positive integer number

---
+++ src/uz/coder/Numbers/PositiveIntegerNumber.java
@@ -0,0 +1,18 @@
+package uz.coder.Numbers;
+
+public class PositiveIntegerNumber {
+    public static void main(String[] args) {
+        int num = 54;
+        int n = num;
+        int rev = 0;
+        while (num > 0) {
+            rev <<= 1;
+            if ((int) (num & 1) == 1)
+                rev^=1;
+                num>>=1;
+
+        }
+        System.out.println("The orginal number is: " + n);
+        System.out.println("The number with reversed bits is: " + rev);
+    }
+}
